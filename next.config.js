292f95537760ae386e4a8173770a6d21b6ef1d03
jeberulz <iseghohi.john@gmail.com>
2025-11-10 13:30:49 +0000

    Update build and manifest files for Next.js, including changes to page structure and added fallback files. Remove unnecessary server development cache files and update webpack runtime hash values.

--- next.config.js
+++ next.config.js
@@ -7,6 +7,15 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: false,
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig
